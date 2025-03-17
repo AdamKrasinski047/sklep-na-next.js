b247114c64c1e4582b1e966c887c45e8763dfb51
Adam Krasinski <2014adam04@gmail.com>
2025-03-17 20:10:29 +0100

    zmiany

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Button } from '@/components/ui/button'
-import Hero from '@/components/home/Hero'
-import FeaturedProducts from '@/components/home/FeaturedProducts'
+import FeaturedProducts from '@/components/home/FeaturedProducts';
+import Hero from '@/components/home/Hero';
+import LoadingContainer from '@/components/global/LoadingContainer';
+import { Suspense } from 'react';
 function HomePage() {
   return (
     <>
-      <Hero/>
-      <FeaturedProducts/>
+      <Hero />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
     </>
-  )
+  );
 }
-
-export default HomePage
+export default HomePage;
